5f5193d4ba806872b2ae5d8f561dd7e046549de0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-29 16:20:19 +0400

    Create MsCurrencyApplication.java

---
+++ src/main/java/com/rustam/ms_currency/MsCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.ms_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrencyApplication.class, args);
+	}
+
+}
