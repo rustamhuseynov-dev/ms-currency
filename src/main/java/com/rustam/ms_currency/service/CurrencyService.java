53c7652d16b926d8a23cfc9eb2c57ee23303e014
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-30 21:01:15 +0400

    Update CurrencyService.java

--- src/main/java/com/rustam/ms_currency/service/CurrencyService.java
+++ src/main/java/com/rustam/ms_currency/service/CurrencyService.java
@@ -1,10 +1,9 @@
 package com.rustam.ms_currency.service;
 
-import com.rustam.ms_currency.dto.CurrencyDto;
-import com.rustam.ms_currency.util.CurrencyProperties;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,11 +17,12 @@ public class CurrencyService {
 
     RedisTemplate<String,Object> redisTemplate;
     RestTemplate restTemplate;
-    CurrencyProperties currencyProperties;
+    @Value("${spring.currency.api-key}")
+    static String apiKey;
 
 
     public void save() {
-        String apiUrl = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_qG9OgrdEqoD2aj2XOnRrZn5kCmI8w5YPFIYaao8Z&currencies=EUR,USD,CAD&base_currency=TRY";
+        String apiUrl = "https://api.freecurrencyapi.com/v1/latest?apikey="+apiKey+"&currencies=EUR,USD,CAD&base_currency=TRY";
 
         Map<String, Object> currencies = restTemplate.getForObject(apiUrl, Map.class);
         Map<String, Double> rates = (Map<String, Double>) currencies.get("data");
